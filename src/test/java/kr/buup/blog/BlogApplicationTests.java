2fe7cf9a629dcb0123511ff8f890f0125a028f48
jooyh <aguitarj@naver.com>
2021-03-20 22:42:35 +0900

    Add backend project for blog
    프로젝트를 추가 했습니다.

---
+++ src/test/java/kr/buup/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.buup.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
